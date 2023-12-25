c88f921b544d57b7e26e2f955cfd65c924c515de
Jiahao Li <jiahaoli0465@gmail.com>
2023-12-25 04:40:05 -0700

    added new form models

--- forms.py
+++ forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, EmailField, TextAreaField, validators
-from wtforms.validators import InputRequired, Email
+from wtforms import StringField, PasswordField, EmailField, TextAreaField, validators,  SelectField, FormField, IntegerField, FloatField
+from wtforms.validators import InputRequired, Email, Optional, Length
+
 
 
 class RegisterForm(FlaskForm):
@@ -25,5 +26,37 @@ class NewWorkType(FlaskForm):
 
      type_name = StringField("Type", validators=[InputRequired()])
      description = TextAreaField('Mailing Address', [validators.optional(), validators.length(max=200)])
+
+class NewExercise(FlaskForm):
+     """Form for creating new unique exercise"""
+
+     name = StringField("Type", validators=[InputRequired()])
+     description = TextAreaField('Mailing Address', [validators.optional(), validators.length(max=200)])
+
+class NewWorkLog(FlaskForm):
+     """Form for creating new worklog"""
+
+     title = StringField("Type", validators=[InputRequired()])
+
+
+
+
+
+class NewExerciseForm(FlaskForm):
+    """Sub-form for creating a new exercise within the worklog edit form."""
+    name = StringField("Exercise Name", validators=[InputRequired()])
+    description = TextAreaField("Description", validators=[Optional(), Length(max=200)])
+
+class EditWorkLog(FlaskForm):
+    """Form for editing a worklog."""
+
+    title = StringField("Title", validators=[InputRequired()])
+    existing_exercise = SelectField("Existing Exercises", coerce=int, choices=[], validators=[Optional()])
+    new_exercise = FormField(NewExerciseForm)
+
+class NewSet(FlaskForm):
+    weight = FloatField("Weights", validators=[InputRequired()])
+    reps = IntegerField("Reps", validators=[InputRequired()])
+    rest_time = IntegerField("Rest time", validators=[Optional()])
 
 
