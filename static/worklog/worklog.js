a87caeb14f9ee65cb001c3cf034ea133ad6c3f99
Jiahao Li <jiahaoli0465@gmail.com>
2023-12-28 03:03:16 -0500

    added new features for ui, updated some

--- static/worklog/worklog.js
+++ static/worklog/worklog.js
@@ -6,41 +6,42 @@ const exitSet = document.querySelector('#exitSet');
 const newSetFormDiv = document.querySelector('#newSetFormDiv');
 const newSetForm = document.querySelector('#newSetForm');
 let logsContainer = document.querySelector('.logsContainer'); 
+let currentExerciseId = null;
+
+// async function sendNewExerciseRequest(exerciseName){
+
+//     console.log(`send ${exerciseName}`)
+//     const options = {
+//         method:"POST", 
+
+//         body:JSON.stringify({
+//             exerciseName:exerciseName
+//         }), 
+
+//         headers: {'Content-type': 'application/json; charset=UTF-8'}
+//     }
     
-async function sendNewExerciseRequest(exerciseName){
+//     const url = "";
+//     const request = new Request(url, options)
 
-    console.log(`send ${exerciseName}`)
-    const options = {
-        method:"POST", 
+//     await fetch(request)
+//     .then((response) => {
+//         if(response.ok){
+//             console.log("Request successful")
 
-        body:JSON.stringify({
-            exerciseName:exerciseName
-        }), 
-
-        headers: {'Content-type': 'application/json; charset=UTF-8'}
-    }
-    
-    const url = "";
-    const request = new Request(url, options)
-
-    await fetch(request)
-    .then((response) => {
-        if(response.ok){
-            console.log("Request successful")
-
-            return response.text()
-        } else {
-            throw new Error(`something bad happened: ${response.status}`);
-        }
-    })
-    .then((data) => {
-        console.log(data)
-        // here you would add something that handles the data from a json preferably but there's nothing here so have a skeleton instead
-        for(let thing in data){
-            //document.getElementById("container for workouts").innerHtml+= "<PUT HTML HERE>"
-        }
-    })
-}
+//             return response.text()
+//         } else {
+//             throw new Error(`something bad happened: ${response.status}`);
+//         }
+//     })
+//     .then((data) => {
+//         console.log(data)
+//         // here you would add something that handles the data from a json preferably but there's nothing here so have a skeleton instead
+//         for(let thing in data){
+//             //document.getElementById("container for workouts").innerHtml+= "<PUT HTML HERE>"
+//         }
+//     })
+// }
 
 document.addEventListener('DOMContentLoaded', async function() { 
         
@@ -57,12 +58,13 @@ document.addEventListener('DOMContentLoaded', async function() {
     });
 
     newSetFormDiv
-    function newExerciseTemplate(exerciseName) {
+    function newExerciseTemplate(exerciseName, exerciseId) {
+        let btnId = exerciseId;
         return `            
         <div class="log">
             <div class="log-container">
-                <div class="log-title"><h4>${exerciseName}</h4> <div class="btns"><button class="add">+</button><button class="dlt">x</button></div></div>
-                <div class="log-content">
+                <div class="log-title"><h4>${exerciseName}</h4> <div class="btns"><button data-btnId = ${btnId} class="add">+</button><button class="dlt">x</button></div></div>
+                <div class="log-content" data-exerciseId = ${exerciseId}>
 
                 </div>
             </div>
@@ -78,28 +80,54 @@ document.addEventListener('DOMContentLoaded', async function() {
 
     
     }
-    
+    // TEMPORARY TESTING
+    let count = 2;
+    /////////////////////
     newExerciseForm.addEventListener('submit', function(e) {
         e.preventDefault();
         let exerciseName = document.querySelector('#exerciseName').value; 
         console.log(exerciseName);
-        const template = newExerciseTemplate(exerciseName);
 
-        // Convert the string to a DOM element
+
+
+
+
+
+
+
+        // let res = sendNewExerciseRequest(exerciseName)
+
+        const template = newExerciseTemplate(exerciseName, count);
+        count += 1;
         const tempDiv = document.createElement('div');
         tempDiv.innerHTML = template.trim(); 
-
-
         logsContainer.appendChild(tempDiv.firstChild);
         addExerciseDiv.classList.add('hidden');
-
-
-        sendNewExerciseRequest(exerciseName)
-
     });
     
     newSetForm.addEventListener('submit', function(e) {
         e.preventDefault();
+        const num = document.querySelector('#set-num').value;
+        const weight = document.querySelector('#set-weight').value;
+        const reps = document.querySelector('#set-rep').value;
+        
+        //send request to server
+
+        const setTemplate = newSetTemplate(num, weight, reps);
+        const tempDiv = document.createElement('div');
+        tempDiv.innerHTML = setTemplate.trim(); 
+        const logContainer = document.querySelector(`[data-exerciseId="${currentExerciseId}"]`);
+    
+        if (logContainer) {
+            // Append the new set to the log-content div
+            console.log('im here')
+            logContainer.appendChild(tempDiv.firstChild);
+        }
+    
+        // Reset the form and currentExerciseId
+        newSetForm.reset();
+        currentExerciseId = null;
+        newSetFormDiv.classList.add('hidden');
     });
 
 
@@ -115,6 +143,7 @@ document.addEventListener('DOMContentLoaded', async function() {
         }
         if (event.target.classList.contains('add')) {
             // Show confirmation dialog
+            currentExerciseId = event.target.getAttribute('data-btnId');
             newSetFormDiv.classList.remove('hidden');
 
                 
