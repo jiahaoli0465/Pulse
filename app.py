153663057312c29a696286c563e214d59a074654
Jiahao Li <jiahaoli0465@gmail.com>
2024-02-19 23:24:17 -0500

    modifying for deployment

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = (
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
-app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = True
+# app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = True
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "it's a secret")
 toolbar = DebugToolbarExtension(app)
 migrate = Migrate(app, db)
